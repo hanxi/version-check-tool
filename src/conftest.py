9340e15c02449ba499639216141aee6d2f92c365
涵曦 <im.hanxi@gmail.com>
2023-02-25 18:55:48 +0800

    修改详情等信息

--- src/conftest.py
+++ src/conftest.py
@@ -1,4 +1,8 @@
 import pytest
+from py.xml import html
+from py.xml import raw
+from ansi2html import Ansi2HTMLConverter
+from html import escape
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -22,7 +26,55 @@ def pytest_html_report_title(report):
     report.title = "版本静态检查报告"
 
 def pytest_html_results_table_header(cells):
-    cells.pop(-1)  # 删除link列
+    cells.pop(1)  # 删除检查列
+    cells.pop(-1) # 删除link列
+    cells.insert(1, html.th("描述"))
 
 def pytest_html_results_table_row(report, cells):
-    cells.pop(-1)  # 删除link列
+    cells.pop(1)  # 删除检查列
+    cells.pop(-1) # 删除link列
+    description = "未知"
+    if hasattr(report, "description"):
+        description = report.description
+    cells.insert(1, html.td(description))
+
+def pytest_html_results_table_html(report, data):
+    del data[:]
+    log = html.div(class_="log")
+    if report.skipped and report.longrepr:
+        arr = report.longreprtext.strip("()").split(",")
+        skip_msg = arr[len(arr)-1]
+        log.append(html.span(raw(escape(skip_msg))))
+    elif report.failed and report.longrepr:
+        pass
+    elif report.longrepr:
+        text = report.longreprtext or report.full_text
+        for line in text.splitlines():
+            separator = line.startswith("_ " * 10)
+            if separator:
+                log.append(line[:80])
+            else:
+                exception = line.startswith("E   ")
+                if exception:
+                    log.append(html.span(raw(escape(line)), class_="error"))
+                else:
+                    log.append(raw(escape(line)))
+            log.append(html.br())
+
+    for section in report.sections:
+        header, content = map(escape, section)
+
+        converter = Ansi2HTMLConverter(
+            inline=False, escaped=False,
+        )
+        content = converter.convert(content, full=False)
+
+        log.append(raw(content))
+        log.append(html.br())
+    data.append(log)
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.description = str(item.function.__doc__)
--- src/init_test.py
+++ src/init_test.py
@@ -5,17 +5,37 @@ g_init_succ = False
 checkinit = pytest.mark.skipif(not g_init_succ, reason='初始化失败')
 
 def test_init(env):
+    '''
+    初始化
+    '''
     global g_init_succ
     print("in test_init")
     if proj.init(env):
         g_init_succ = True
     else:
-        assert False, "初始化环境失败"
+        assert False
     print("init_succ", g_init_succ)
 
+def test_succ():
+    '''
+    测试通过
+    '''
+    print("succ")
+    pass
+
+def test_failed(env):
+    '''
+    测试失败
+    '''
+    print("failed")
+    assert False
+    pass
 
 @checkinit
 def test_need_init():
+    '''
+    测试依赖初始化
+    '''
     print("g_init_succ:", g_init_succ)
     print("succ")
     pass
